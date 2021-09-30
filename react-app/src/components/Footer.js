f34a9150aec1203f4b49eb32e03c303b1b5623b5
aAJacob <wellerjay118@gmail.com>
2021-09-30 18:25:16 -0500

    added repo link to footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -3,17 +3,23 @@ import React from "react";
 const Footer = () => {
 const linkedIn = "https://www.linkedin.com/in/jacob-weller-592795161/"
 const github = "https://github.com/WellerJay118"
+const repo = "https://github.com/WellerJay118/capStone"
 
     return (
         <div>
             <button>
                 <a target="_blank" rel="noreferrer" href={github}>
-                    <i className="fab fa-github"></i>
+                    <i className="fab fa-github fa-3x"></i>
                 </a>
             </button>
             <button>
                 <a target="_blank" rel="noreferrer" href={linkedIn}>
-                    <i className="fab fa-linkedin"></i>
+                    <i className="fab fa-linkedin fa-3x"></i>
+                </a>
+            </button>
+            <button>
+                <a target="_blank" rel="noreferrer" href={repo}>
+                    <i className="far fa-file-code fa-3x"></i>
                 </a>
             </button>
         </div>
