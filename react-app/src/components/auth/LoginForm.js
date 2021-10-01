227b44a98fce5053e142d28091a4cb2d77d14ca6
aAJacob <wellerjay118@gmail.com>
2021-10-01 07:23:55 -0500

    sleep push

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -37,38 +37,38 @@ const LoginForm = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={onLogin}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-            ))}
-        </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-            />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-            />
-          <button type='submit'>Login</button>
-          <button onClick={demoLogin}>Demo</button>
-        </div>
-      </form>
-
-      {/* <Footer /> */}
+    <div className="login__wrapper">
+      <div>
+        <form onSubmit={onLogin}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+              ))}
+          </div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+              />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+              />
+            <button type='submit'>Login</button>
+            <button onClick={demoLogin}>Demo</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
