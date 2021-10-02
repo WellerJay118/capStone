48ba7d0facb916101c4d2e100efdf65bc4dcfc61
aAJacob <wellerjay118@gmail.com>
2021-10-02 13:26:38 -0500

    some validation and styling

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -20,9 +20,9 @@ def password_matches(form, field):
     email = form.data['email']
     user = User.query.filter(User.email == email).first()
     if not user:
-        raise ValidationError('No such user exists.')
+        raise ValidationError('Invalid Email or Password.')
     if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+        raise ValidationError('Incorrect password.')
 
 
 class LoginForm(FlaskForm):
