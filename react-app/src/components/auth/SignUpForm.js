599f63158710cdc47bc6791b348b206d649f7f1b
aAJacob <wellerjay118@gmail.com>
2021-10-04 01:19:43 -0500

    styled sign up

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -57,67 +57,80 @@ const SignUpForm = () => {
 
   return (
     <div className="signup__wrapper">
-      <div className="signup__container:">
+
+      <div className="signupPage__container">
+
+        <div className="signupPage__errors">
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+
         <form onSubmit={onSignUp}>
-          <div>
-            {errors.map((error, ind) => (
-              <div key={ind}>{error}</div>
-            ))}
-          </div>
-          <div>
-            <label>User Name</label>
+          {/* <div className="signupPage__form--username-input"> */}
+            {/* <label>Username</label> */}
             <input
+            className="signupPage__form--input"
+              placeholder="Username"
               type='text'
               name='username'
               onChange={updateUsername}
               value={username}
-            ></input>
-          </div>
-          <div>
-            <label>Email</label>
+            />
+          {/* </div> */}
+            {/* <label>Email</label> */}
             <input
+              className="signupPage__form--input"
+              placeholder="Email address"
               type='text'
               name='email'
               onChange={updateEmail}
               value={email}
-            ></input>
-            <label>First Name</label>
+            />
+          {/* <div className="signupPage__form--firstname-input"> */}
+            {/* <label>First Name</label> */}
             <input
+              className="signupPage__form--input"
+              placeholder="First Name"
               type='text'
               name='firstName'
               onChange={updateFirstName}
               value={firstName}
-            ></input>
-            <label>Last Name</label>
+            />
+          {/* </div> */}
+            {/* <label>Last Name</label> */}
             <input
+              className="signupPage__form--input"
+              placeholder="Last Name"
               type='text'
               name='lastName'
               onChange={updateLastName}
               value={lastName}
-            ></input>
-          </div>
-          <div>
-            <label>Password</label>
+            />
+            {/* <label>Password</label> */}
             <input
+              className="signupPage__form--input"
+              placeholder="Password"
               type='password'
               name='password'
               onChange={updatePassword}
               value={password}
-            ></input>
-          </div>
-          <div>
-            <label>Repeat Password</label>
+            />
+            {/* <label>Confirm Password</label> */}
             <input
+              className="signupPage__form--input"
+              placeholder="Confirm Password"
               type='password'
               name='repeat_password'
               onChange={updateRepeatPassword}
               value={repeatPassword}
               required={true}
-            ></input>
-          </div>
-          <button type='submit'>Sign Up</button>
+            />
+          <button className="signupPage__form--button"type='submit'>Sign Up</button>
         </form>
+
       </div>
+
     </div>
   );
 };
