7d279526033d5e7efd871f84d821a6ecc271b20e
aAJacob <wellerjay118@gmail.com>
2021-10-19 11:04:42 -0500

    bottom borders for splash

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -7,8 +7,8 @@ const SplashPage = () => {
     if (sessionUser) history.push('/projects')
 
     return(
-        <div className="splash-wrapper">
-            <div className="splash-container">
+        <div className="splash-container">
+            <div className="splash-wrapper">
                 <div className="splash-gridrow">
                     <div className="splash-gridrow__image">
                         {/* <h2>Image here</h2> */}
@@ -18,6 +18,7 @@ const SplashPage = () => {
                         <p>this is where alot of our text will go for the things and stuff we want to showcase from our first image</p>
                     </div>
                 </div>
+                    <div className="splash-gridrow__border"></div>
                 <div className="splash-gridrow">
                     <div className="splash-gridrow__text">
                         <p>this is where alot of our text will go for the things and stuff we want to showcase from our second image</p>
@@ -27,6 +28,7 @@ const SplashPage = () => {
                         <img alt="" src="https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"></img>
                     </div>
                 </div>
+                    <div className="splash-gridrow__border"></div>
                 <div className="splash-gridrow">
                     <div className="splash-gridrow__image">
                         {/* <h2>Image here</h2> */}
@@ -36,6 +38,7 @@ const SplashPage = () => {
                         <p>this is where alot of our text will go for the things and stuff we want to showcase from our third image</p>
                     </div>
                 </div>
+                    <div className="splash-gridrow__border"></div>
                 <div className="splash-gridrow">
                     <div className="splash-gridrow__text">
                         <p>this is where alot of our text will go for the things and stuff we want to showcase from our fourth image</p>
@@ -45,6 +48,7 @@ const SplashPage = () => {
                         <img alt="" src="https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"></img>
                     </div>
                 </div>
+                    <div className="splash-gridrow__border"></div>
             </div>
 
         </div>
